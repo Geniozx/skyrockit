0f29840208a683d3095daa814b57fa685525e85b
GenioZx <eli.rodri14@email.com>
2025-07-24 17:39:50 -0400

    fixed all bugs

--- controllers/applications.js
+++ controllers/applications.js
@@ -27,6 +27,11 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+router.get('/new', async (req, res) => {
+  res.render('applications/new.ejs');
+});
+
 // controllers/applications.js
 
 router.get('/:applicationId', async (req, res) => {
@@ -45,6 +50,27 @@ router.get('/:applicationId', async (req, res) => {
     res.redirect('/');
   }
 });
+
+// controllers/applications.js`
+
+router.post('/', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the
+    // applications array of the current user
+    currentUser.applications.push(req.body);
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 
 // controllers/applications.js
 
@@ -67,16 +93,16 @@ router.delete('/:applicationId', async (req, res) => {
 });
 
 router.get('/:applicationId/edit', async (req, res) => {
-    try {
-        const currentUser = await User.findById(req.session.user._id);
-        const application = currentUser.applications.id(req.params.applicationId);
-        res.render('applications/edit.ejs', {
-            application: application,
-        });
-    } catch (error) {
-        console.log(error);
-        res.redirect('/');
-    }
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/edit.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 // controllers/applications.js`
